bf64759d0372d9a0a41ae07bfd9bb4400bf1ef15
Haris Masovic <hmasovic1@etf.unsa.ba>
2019-04-13 00:22:56 +0200

    dodani unit testovi

--- README.md
+++ README.md
@@ -28,7 +28,8 @@ Kolaboracija predstavlja modul koji omogućava asistentima i studentima bolju or
 - Mogućnost bodovanja projekata,
 - Mogućnost pregleda rada na projektima.
 
-## Port klasifikacija & Runtime/Testing & envoirment
+## Port klasifikacija & Runtime/Testing & envoirment upute
+- `npm install` uraditi unutar backend foldera
 - 3001 : Rest api node
 - `npm start` u backend folderu za pokreteanje servisa
 - `npm test` u backend folderu za rest api testove
--- backend-kb/tests/student/workTests.js
+++ backend-kb/tests/student/workTests.js
@@ -3,7 +3,11 @@ const request = require('request');
 const dotenv = require('dotenv'); // definisanje env varijabli
 dotenv.config(); // postavljanje configa 
 
+const uuidv4 = require('uuid/v4');
+const connection = require('../../db').connection;
+
 describe('Testiranje post metode base/api/work', () => {
+    
 
     it('Treba da vraca specifican json jer nije zadovoljen body', (done) => {
         request.post({
@@ -13,34 +17,83 @@ describe('Testiranje post metode base/api/work', () => {
             url: `${process.env.FULL_NAME}/api/work`
         }, function (error, response, body) {
             expect(body).to.equal(JSON.stringify({
-                message: 'Body parametri nisu specifirani [id_projekta, prioritet, od_kad, do_kad]'
+                message: 'Body parametri nisu specifirani [id_projekta, od_kad, do_kad]'
             }));
             done();
         });
     });
-
-    it('Treba da vraca jer je zadovoljen body', (done) => {
+    
+    it('Treba da vrati gresku da datumi nisu uredu!', (done) => {
         request.post({
             headers: {
                 'content-type': 'application/x-www-form-urlencoded'
             },
             url: `${process.env.FULL_NAME}/api/work`,
-            body:    "id_projekta=1&prioritet=0.2&od_kad=danas&do_kad=sutra"
+            body:    encodeURI("id_projekta=3&od_kad=2019-14-03 23:00:00&do_kad=2019-12-03 23:00:00") // u bazi je dummy 3
         }, function (error, response, body) {
-            let novi = JSON.parse(body);
-            expect(novi.id_projekta).to.equal('1');
-            expect(novi.prioritet).to.equal('0.2');
-            expect(novi.od_kad).to.equal('danas');
-            expect(novi.do_kad).to.equal('sutra');
+            expect(body).to.equal(JSON.stringify({
+                message: 'Datumi nisu u formatu [yyyy-mm-dd hh:mm:ss]!'
+            }));
+            done();
+        });
+    });
+    
+    it('Treba da vraca isti body koji je poslan, jer je zadovoljeno sve', (done) => {
+        request.post({
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded'
+            },
+            url: `${process.env.FULL_NAME}/api/work`,
+            body:    encodeURI("id_projekta=55&od_kad=2019-11-03 23:00:00&do_kad=2019-12-03 23:00:00") // u bazi je dummy 3
+        }, function (error, response, body) {
+            expect(body).to.equal(JSON.stringify({
+                message: 'Poslani [id_projekta] ne postoji u bazi ili je doslo do greske sa bazom!'
+            }));
             done();
         });
     });
 
-    it('Treba da unese u bazu novi projektni zadatak', (done) => {    
-        // fali 1 test za testiranje jel se dodaju u bazu
-        // bice kad se vođe dogovore !
-        expect(true).to.equal(true);
-        done();
+    
+    it('Treba da vrati gresku jer id_projekta nije zaodovljen', (done) => {
+        request.post({
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded'
+            },
+            url: `${process.env.FULL_NAME}/api/work`,
+            body:    encodeURI("id_projekta=3&od_kad=2019-11-03 23:00:00&do_kad=2019-12-03 23:00:00") // u bazi je dummy 3
+        }, function (error, response, body) {
+            let novi = JSON.parse(body);
+            expect(novi.id_projekta).to.equal('3');
+            expect(novi.od_kad).to.equal('2019-11-03 23:00:00');
+            expect(novi.do_kad).to.equal('2019-12-03 23:00:00');
+            done();
+        });
     });
+
+    
+    it('Treba da unese u bazu novi projektni zadatak pod id_projektom 3', (done) => {  
+        let random = uuidv4();    
+        request.post({
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded'
+            },
+            url: `${process.env.FULL_NAME}/api/work`,
+            body:    encodeURI(`id_projekta=3&od_kad=2019-11-03 23:00:00&do_kad=2019-12-03 23:00:00&opis=${random}`) 
+            // u bazi je dummy 3
+        }, function (error, response, body) {
+            let novi = JSON.parse(body);
+
+            connection.query(`SELECT * FROM ProjektniZadatak WHERE opis='${random}'`, (error, results1, fields) => {
+                expect(novi.id_projekta).to.equal('3');
+                expect(novi.od_kad).to.equal('2019-11-03 23:00:00');
+                expect(novi.do_kad).to.equal('2019-12-03 23:00:00');
+
+                let provjeraBaze = JSON.parse(JSON.stringify(results1));
+                expect(provjeraBaze.length).to.equal(1);
+                expect(provjeraBaze[0].opis).to.equal(random);
+                done();
+            });
+        });
+    }); 
 
 });
