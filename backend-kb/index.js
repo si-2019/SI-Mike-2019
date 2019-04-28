5aae5ebae16d9ca59958a8ddb87173395d22b310
lamijaalagic <lalagic1@etf.unsa.ba>
2019-04-29 01:03:45 +0200

    dodana ruta za novu komponentu

--- backend-kb/index.js
+++ backend-kb/index.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv'); // definisanje env varijabli
-dotenv.config(); // postavljanje configa 
+const dotenv = require('dotenv');  // definisanje env varijabli
+dotenv.config();                   // postavljanje configa 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const db = require('./models/db.js');
+const db = require ('./models/db.js');
 
 // povezivanje sa bazom
 db.sequelize.sync()
     .then(() => console.log("Uspjesno povezano sa peca bazom!"))
     .catch((err) => console.log("Nije uspjesno povezano sa peca bazom!", err));
 
+
 // default parametars za rad sa slanjem podataka
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
+  
 // definisanje REST API calls / ruta
 
-const workRouter = require('./routes/students/workRoute')
+const workRouter = require('./routes/students/workRoute');
 const projectsRouter = require('./routes/asistants/projectsRoute');
+const groupRouter = require('./routes/students/groupRoute');
 
 // postavljanje CORS-a za naš drugi server
 // da samo on može kupiti podatke
@@ -35,7 +37,7 @@ app.use('/*', (req, res, next) => {
 // definisanje 4 rutera, za definisanje api calls za studenta
 
 // definisanje ruta za dio "Kreiranje projektne grupe"
-// app.use('/group', projectRouter); 
+app.use('/api/group', groupRouter); 
 // definisanje ruta za dio "Pregled projekata"
 // app.use('/view', projectRouter);
 // definisanje ruta za dio "Rad na projektu"
@@ -46,13 +48,17 @@ app.use('/api/work', workRouter);
 // definisanje 4 rutera, za definisanje api calls za asistenta
 
 // definisanje ruta za dio "Kreiranje projekata na nivou predmeta"
-app.use('/api/projects', projectsRouter);
+app.use('/api/projects', projectsRouter); 
 // definisanje ruta za dio "Generisanje projektnih grupa"
 // app.use('/group', projectRouter); 
 // definisanje ruta za dio "Bodovanje projekata"
 // app.use('/group', projectRouter); 
 // definisanje ruta za dio "Pregled projekata"
 // app.use('/group', projectRouter);
+
+// konektovanje na bazu
+const konekt = require('./db');
+konekt.konektuj();
 
 app.listen(PORT, () => {
     console.log(`Rest-api service started on ${PORT} port!`);
