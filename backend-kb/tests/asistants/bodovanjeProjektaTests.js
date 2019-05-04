7c6e8f2a907602ccf20c5b1aadaa0e43886af123
eskopljak <emin.skopljak@me.com>
2019-05-04 18:26:14 +0200

    Dodani unit testovi za bodovanje projekta

---
+++ backend-kb/tests/asistants/bodovanjeProjektaTests.js
@@ -0,0 +1,92 @@
+const expect = require('chai').expect;
+const request = require('request');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const db = require('../../models/db');
+
+describe('Testiranje post metode base/api/projects/bodovanje', () => {
+
+    it('Treba da vraca gresku jer nije zadovoljen body', (done) => {
+        request.post({
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded'
+            },
+            url: `${process.env.FULL_NAME}/api/projects/bodovanje`
+        }, function (error, response, body) {
+            expect(body).to.equal(JSON.stringify({
+                message: 'Body parametri nisu specifirani [idGrupaProjekta, bodovi]'
+            }));
+            done();
+        });
+    });
+
+    it('Treba da vrati gresku jer idGrupaProjekta ne postoji u bazi ', (done) => {
+        request.post({
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded'
+            },
+            url: `${process.env.FULL_NAME}/api/projects/bodovanje`,
+            body: encodeURI("idGrupaProjekta=15678&bodovi=10")
+        }, function (error, response, body) {
+            expect(body).to.equal(JSON.stringify({
+                message: 'Pogresan ID grupe.'
+            }));
+            done();
+        });
+    });
+
+    it('Treba da vrati gresku jer su bodovi manji od nule', (done) => {
+        request.post({
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded'
+            },
+            url: `${process.env.FULL_NAME}/api/projects/bodovanje`,
+            body: encodeURI("idGrupaProjekta=1&bodovi=-1")
+        }, function (error, response, body) {
+            expect(body).to.equal(JSON.stringify({
+                message: 'Bodovi moraju biti u intervalu [0, max].'
+            }));
+            done();
+        });
+    });
+
+    it('Treba da vrati gresku jer su bodovi veci od maksimalnih za projekat', (done) => {
+        request.post({
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded'
+            },
+            url: `${process.env.FULL_NAME}/api/projects/bodovanje`,
+            body: encodeURI("idGrupaProjekta=2&bodovi=10000")
+        }, function (error, response, body) {
+            expect(body).to.equal(JSON.stringify({
+                message: 'Bodovi moraju biti u intervalu [0, max].'
+            }));
+            done();
+        });
+    });
+
+    it('Treba da ispravno izvrsi upis bodova projekta i komentara asistenta u bazu', (done) => {
+        request.post({
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded'
+            },
+            url: `${process.env.FULL_NAME}/api/projects/bodovanje`,
+            body: encodeURI("idGrupaProjekta=1&bodovi=8&komentar=prvi_komentar")
+        }, function (error, response, body) {
+            expect(body).to.equal(JSON.stringify({
+                message: 'Uspjesno bodovan projekat.'
+            }));
+
+            db.GrupaProjekta.findOne({
+                where: {
+                    idGrupaProjekta: 1
+                }
+            }).then((rez) => {
+                expect(rez.ostvareniBodovi).to.equal(8);
+                expect(rez.komentarAsistenta).to.equal('prvi_komentar');
+                done();
+            })
+        });
+    });
+});
