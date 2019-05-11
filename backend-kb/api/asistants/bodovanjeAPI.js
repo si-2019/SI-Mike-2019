10ccd4ef07aaadfc9b75dc78f2e2ab4b809a241d
eskopljak <emin.skopljak@me.com>
2019-05-11 23:33:19 +0200

    Dodani swagger opisi

--- backend-kb/api/asistants/bodovanjeAPI.js
+++ backend-kb/api/asistants/bodovanjeAPI.js
@@ -6,8 +6,35 @@ const bodovanjeRouter = express.Router();
  * /api/bodovanjeprojekata/unified:
  *    post:
  *      tags:
-*       - Asistenti - Bodovanje projekata - API
- *      description: Omogucava bodovanje projekata, tako da se definisu bodovi jednaki za svakog člana  
+ *       - Asistenti - Bodovanje projekata - API
+ *      description: 'Omogucava bodovanje projekata, tako da se definisu bodovi jednaki za svakog clana.
+ *      Salje se kao url encoded format i prima id projektne grupe i broj bodova, 
+ *      te vraca poruku u skladu s tim da li su bodovi uspjesno postavljeni ili je doslo do greske.
+ *      Realizovano od strane: Skopljak Emin'
+ *      consumes:
+ *       - application/x-www-form-urlencoded
+ *      parameters:
+ *        - in: formData
+ *          name: idGrupaProjekta
+ *          type: integer
+ *          description: ID projektne grupe ciji clanovi se boduju.
+ *        - in: formData
+ *          name: bodovi
+ *          type: integer
+ *          description: Broj bodova koji su ostvarili clanovi grupe.
+ *      required:
+ *        - idGrupaProjekta
+ *        - bodovi
+ *      responses:
+ *       200:
+ *         description: Vraca se JSON objekat sa parametrom message
+ *         content: 
+ *           application/json:
+ *               schema: 
+ *                 type: object
+ *                 properties:
+ *                  message:
+ *                   type: string       
  */
 bodovanjeRouter.post('/unified', (req, res) => res.redirect(307, `/services/bodovanjeprojekata/unified`));
 
