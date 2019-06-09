51915b6e0db0446ff51ebc6fe1c3b734f33445ed
Haris Masovic <hmasovic1@etf.unsa.ba>
2019-06-09 23:57:11 +0200

    fix?

--- backend-kb/swaggerDoc.js
+++ backend-kb/swaggerDoc.js
@@ -10,7 +10,7 @@ const options = {
             version: '1.0.0', 
             description: 'Citav opis potreban za backend dio za tim MIKE koji se tiče opisa i testiranje metoda za API i SERVISE.',
         },
-        host : `${process.env.HOST}:${process.env.PORT}`,
+        host : `${process.env.HOST}`,
         basePath: '/',
         servers : [ {
             url : process.env.FRONTEND,
